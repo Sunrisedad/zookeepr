0de3132d5afd5ca4e47b78eb0c5dcb63f46c723c
Sheldon Collins <zane21@gmail.com>
2021-09-28 14:27:11 -0600

    updated port number

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { animals } = require('./data/animals.json');
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, animalsArray) {
@@ -41,16 +42,13 @@ function filterByQuery(query, animalsArray) {
   return filteredResults;
 }
 
-app.get('/api/animals', (req, res) => {
-  let results = animals;
-  if (req.query) {
-    results = filterByQuery(req.query, results);
-  }
-  res.json(results);
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+    res.json(result);
 });
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
-  });
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
 
   
